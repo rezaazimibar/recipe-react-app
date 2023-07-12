af1e91fe01a58bcd709e089ad48b81bbb8ff254e
rezaazimibar <www.beta04@gmail.com>
2023-07-12 23:24:09 +0430

    add vegetarian curoser

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -11,32 +11,41 @@ const Popular = () => {
     getPopular();
   }, []);
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${url}&number=10`
-    );
-    const data = await api.json();
-    setPopular(data.recipes);
+    const check = localStorage.getItem("popular");
+
+    if (check) {
+      setPopular(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${url}&number=10`
+      );
+      const data = await api.json();
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+      setPopular(data.recipes);
+      console.log("popular", popular);
+    }
   };
-  console.log("sdfsdf", popular);
+
   return (
     <>
       <Wrapped>
-        <h3>popular picks</h3>
+        <h3>our popular picks</h3>
         <Splide
           options={{
-            perPage: 3,
+            perPage: 4,
             arrows: false,
             pagination: false,
             drag: "free",
-            gap: "5rem",
+            gap: "2rem",
           }}
         >
           {popular.map((recipes) => {
             return (
-              <SplideSlide>
+              <SplideSlide key={recipes.id}>
                 <Card>
-                  <div>{recipes.title}</div>
+                  <p>{recipes.title}</p>
                   <img src={recipes.image} alt={recipes.title} />
+                  <Gradient />
                 </Card>
               </SplideSlide>
             );
@@ -51,14 +60,41 @@ const Wrapped = styled.div`
   margin: 4rem 0rem;
 `;
 const Card = styled.div`
-  min-height: 25rem;
+  min-height: 15rem;
   border-radius: 2rem;
   overflow: hidden;
-  position:relative
+  position: relative;
   img {
     border-radius: 2rem;
-    position:absolute
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-wight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
 
 export default Popular;
--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,8 +1,99 @@
-const Veggie=()=>{
-    return(
-        <>
-        </>
-    )
-}
+import { useEffect, useState } from "react";
+import { url } from "./URL";
 
-export default Veggie
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+
+const Veggie = () => {
+  const [veggie, setVeggie] = useState([]);
+  useEffect(() => {
+    getVeggie();
+  }, []);
+  const getVeggie = async () => {
+    const check = localStorage.getItem("veggie");
+
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${url}&number=10&tags=vegetarian`
+      );
+      const data = await api.json();
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+      setVeggie(data.recipes);
+      console.log("veggie", veggie);
+    }
+  };
+
+  return (
+    <>
+      <Wrapped>
+        <h3>our vegetarian picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "2rem",
+          }}
+        >
+          {veggie.map((recipes) => {
+            return (
+              <SplideSlide key={recipes.id}>
+                <Card>
+                  <p>{recipes.title}</p>
+                  <img src={recipes.image} alt={recipes.title} />
+                  <Gradient />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapped>
+    </>
+  );
+};
+const Wrapped = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 15rem;
+  border-radius: 2rem;
+  overflow: hidden;
+  position: relative;
+  img {
+    border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  p {
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-wight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
+`;
+
+export default Veggie;
