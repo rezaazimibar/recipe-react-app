722ff7176772a715b5154561845c29ec7cee52b7
rezaazimibar <www.beta04@gmail.com>
2023-08-28 11:40:42 +0430

    add back home logo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import Category from "./components/Category";
 import { BrowserRouter } from "react-router-dom";
 // import "./App.css";
 import Search from "./components/Search";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { GiKnifeFork } from "react-icons/gi";
 
 const App = () => {
   return (
     <>
       <div className="App">
         <BrowserRouter>
+          <Nav>
+            <GiKnifeFork />
+            <Logo to={"/"}>delivery</Logo>
+          </Nav>
           <Search />
           <Category />
           <Pages />
@@ -17,5 +24,20 @@ const App = () => {
     </>
   );
 };
+const Logo = styled(Link)`
+  font-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster two", cursive;
+`;
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
